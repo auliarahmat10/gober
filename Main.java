95940481e103e19fe68bd0b8d0231415602c437f
auliarahmat10 <71375988+auliarahmat10@users.noreply.github.com>
2020-09-17 06:29:40 +0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	
+	public static void main(String[] args){
+		System.out.println("Nama : Aulia rahmat");
+		System.out.println("Nim  : 6706202074");
+		System.out.println("kelas: D3IF 44-04");
+	}
+}
